04d029f22fa583b41478717785e41dd228b9319b
serge <serge.hulne@gmail.com>
2025-01-29 18:59:16 +0100

    Auth

--- routes/routes.go
+++ routes/routes.go
@@ -14,9 +14,9 @@ func SetupRouter() *gin.Engine {
 
 	// Routes for viewing, editing, and searching wiki pages
 	router.GET("/", func(c *gin.Context) { c.Redirect(302, "/home") })
-	router.GET("/:title", handlers.ShowPage)
-	router.GET("/:title/edit", handlers.EditPage)
-	router.POST("/:title/save", handlers.SavePage)
+	router.GET("/:title", handlers.AuthMiddleware(), handlers.ShowPage)
+	router.GET("/:title/edit", handlers.AuthMiddleware(), handlers.EditPage)
+	router.POST("/:title/save", handlers.AuthMiddleware(), handlers.SavePage)
 	router.GET("/search", handlers.SearchPage)
 	router.GET("/register", handlers.ShowRegisterPage)
 
